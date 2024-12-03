ef9ea5814124c471b5320508a6099f95233b4a03
zafar-m3n <thezafar.m3n@gmail.com>
2024-12-03 20:51:46 +0530

    Components

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "./../redux/features/alertSlice";
+import { setUser } from "./../redux/features/userSlice";
+import API from "./../services";
+
+export default function ProtectedRoute({ children }) {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+
+  //getUser
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        dispatch(showLoading());
+        const response = await API.private.getUserData({
+          token: localStorage.getItem("token"),
+        });
+        dispatch(hideLoading());
+        if (response.data.success) {
+          dispatch(setUser(response.data.data));
+        } else {
+          <Navigate to="/auth/login" />;
+          localStorage.clear();
+        }
+      } catch (error) {
+        localStorage.clear();
+        dispatch(hideLoading());
+        console.log(error);
+      }
+    };
+    if (!user) {
+      getUser();
+    }
+  }, [user, dispatch]);
+
+  if (localStorage.getItem("token")) {
+    return children;
+  } else {
+    return <Navigate to="/auth/login" />;
+  }
+}
