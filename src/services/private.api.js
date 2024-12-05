804a7ee33342d4d5a9b1abd4547f751425bfaa91
zafar-m3n <thezafar.m3n@gmail.com>
2024-12-05 19:28:31 +0530

    Get User Data

--- src/services/private.api.js
+++ src/services/private.api.js
@@ -1,5 +1,13 @@
 import instance from "../lib/axios";
 
-const privateAPI = {};
+const getUserData = async (params = {}) => {
+  return await instance.client.post("api/v1/user/getUserData", params, {
+    headers: instance.defaultHeaders(),
+  });
+};
+
+const privateAPI = {
+  getUserData,
+};
 
 export default privateAPI;
