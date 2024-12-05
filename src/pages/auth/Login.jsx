67c212aaef4c71e25ac2130ffcdc7d3c64b02803
zafar-m3n <thezafar.m3n@gmail.com>
2024-12-05 16:24:41 +0530

    Login Page fixed

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { showLoading, hideLoading } from "@/redux/features/alertSlice";
 import { message } from "antd";
+import Logo from "@/assets/logo.png";
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -21,17 +22,17 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <header className="bg-[#19376d] text-center text-[#a5d7e8] py-4 px-8 flex items-center">
-        <img src="/images/logo.png" alt="logo" className="h-[100px] mr-5" />
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-blue-900 text-center text-blue-100 py-2 px-8 flex items-center space-x-4">
+        <img src={Logo} alt="logo" className="h-12" />
         <h1 className="text-2xl font-bold">APIIT InternConnect</h1>
       </header>
 
-      <div
-        className="flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat py-5 px-5"
+      <main
+        className="flex flex-col flex-grow justify-center items-center bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: 'url("/images/internship.jpg")' }}
       >
-        <div className="bg-[#a5d7e8] bg-opacity-90 w-4/5 lg:w-2/3 xl:w-1/2 rounded-lg p-6 mb-6 font-semibold text-[#19376d] text-center">
+        <div className="bg-blue-100 bg-opacity-90 w-4/5 lg:w-2/3 xl:w-1/2 rounded-lg p-6 mb-6 font-semibold text-blue-900 text-center">
           <p>
             <strong>Dear Students,</strong>
           </p>
@@ -54,19 +55,23 @@ const Login = () => {
           <p>Please use the chat window to contact ICT department.</p>
         </div>
 
-        <div className="bg-[#a5d7e8] bg-opacity-90 w-4/5 lg:w-2/3 xl:w-1/2 rounded-lg p-6 mb-6 font-semibold text-[#19376d]">
+        <div className="bg-blue-100 bg-opacity-90 w-4/5 lg:w-2/3 xl:w-1/2 rounded-lg p-6 mb-6 font-semibold text-blue-900">
           <p>Cookies must be enabled in your browser</p>
           <p>Log into your InternConnect account via:</p>
           <button
             onClick={performLogin}
-            className="bg-[#007bff] text-white py-2 px-4 rounded-md hover:bg-[#0056b3] flex items-center justify-center mx-auto my-4 transition duration-150"
+            className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 flex items-center justify-center mx-auto my-4 transition duration-150"
           >
-            <img src="/images/office_logo.png" alt="" className="mr-3" />
+            <img
+              src="/images/office_logo.png"
+              alt="Office Logo"
+              className="mr-3"
+            />
             MICROSOFT OFFICE 365 LOGIN
           </button>
         </div>
 
-        <div className="bg-[#a5d7e8] bg-opacity-90 w-4/5 lg:w-2/3 xl:w-1/2 rounded-lg p-6 mb-6 font-semibold text-[#19376d]">
+        <div className="bg-blue-100 bg-opacity-90 w-4/5 lg:w-2/3 xl:w-1/2 rounded-lg p-6 mb-6 font-semibold text-blue-900">
           <h3 className="text-lg font-light">Is this your first time here?</h3>
           <p>
             Students may get their password by logging in to&nbsp;
@@ -81,9 +86,9 @@ const Login = () => {
             .
           </p>
         </div>
-      </div>
+      </main>
 
-      <footer className="bg-[#19376d] text-center text-[#a5d7e8] py-4">
+      <footer className="bg-blue-900 text-center text-blue-100 py-4">
         <p className="text-lg font-semibold">
           All rights reserved 2024 | APIIT ICT Department | E - it@apiit.lk | T
           - Colombo City Campus 0117675120/123 , Law School 0117675216 | W -
